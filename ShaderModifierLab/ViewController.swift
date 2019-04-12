c41463f765bd42587f06ad13b88d474a97939312
mikael.deurell <mikael@deurell.net>
2019-04-12 16:06:07 +0200

    classic demofx

--- ShaderModifierLab/ViewController.swift
+++ ShaderModifierLab/ViewController.swift
@@ -1,27 +1,42 @@
-//
-//  ViewController.swift
-//  ShaderModifierLab
-//
-//  Created by Mikael Deurell on 2019-04-12.
-//  Copyright © 2019 Mikael Deurell. All rights reserved.
-//
-
 import Cocoa
+import SceneKit
 
 class ViewController: NSViewController {
-
+    
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scnView = self.view as? SCNView
+        scnView.showsStatistics = true
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = false
+        
+        scnScene = SCNScene()
+        scnScene.background.contents = NSColor.gray
+        scnView.scene = scnScene
+        
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x:0, y:0, z:12)
+        scnScene.rootNode.addChildNode(cameraNode)
+        
+        let plane = SCNPlane(width: 10, height: 10)
+        
+        guard let shaderURL = Bundle.main.url(forResource: "frag", withExtension: "shader"),
+            let modifier = try? String(contentsOf: shaderURL)
+            else { fatalError("Can't load shader from bundle.") }
+        
+        plane.shaderModifiers = [.fragment: modifier]
+        
+        let node = SCNNode(geometry: plane)
+        plane.firstMaterial?.diffuse.contents = NSImage(named: "skull")
+        plane.firstMaterial?.diffuse.wrapS = SCNWrapMode.repeat;
+        plane.firstMaterial?.diffuse.wrapT = SCNWrapMode.repeat;
+        scnScene.rootNode.addChildNode(node)
+        scnView.isPlaying = true
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
