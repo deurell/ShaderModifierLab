66487f41bc459e553f7c354c101ad39098c23b82
mikael.deurell <mikael@deurell.net>
2019-04-12 13:26:19 +0200

    Initial Commit

---
+++ ShaderModifierLab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShaderModifierLab
+//
+//  Created by Mikael Deurell on 2019-04-12.
+//  Copyright © 2019 Mikael Deurell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ShaderModifierLab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShaderModifierLab
+//
+//  Created by Mikael Deurell on 2019-04-12.
+//  Copyright © 2019 Mikael Deurell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
